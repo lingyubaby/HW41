ae773a13214ca8c9b213599469adc280c8c63c35
lingyuc <lingyuc@umich.edu>
2019-11-12 09:17:24 -0500

    this is commit

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    //commit uu
+    /* commit uu */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
